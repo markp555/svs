2f341c78763e479d80a330cdee2c6cae719ef75a
markp555 <madmark555@gmail.com>
2026-06-11 13:29:30 +0700

    added libraries

--- svslib/svslib.cpp
+++ svslib/svslib.cpp
@@ -1,9 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+//  SVS - Simple Versioning System                                         //
+//  Copyright (C) 2026 Mark_Pr                                             //
+//                                                                         //
+//  This program is free software; you can redistribute it and/or modify   //
+//  it under the terms of the GNU General Public License as published by   //
+//  the Free Software Foundation; either version 2 of the License, or      //
+//  (at your option) any later version.                                    //
+//                                                                         //
+//  This program is distributed in the hope that it will be useful,        //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           //
+//  GNU General Public License for more details.                           //
+//                                                                         //
+//  You should have received a copy of the GNU General Public License      //
+//  along with this program; if not, write to the Free Software            //
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.              //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
 // svslib.cpp : Defines the functions for the static library.
 //
 
 #include "framework.h"
 
-// TODO: This is an example of a library function
-void fnsvslib()
-{
-}
+
